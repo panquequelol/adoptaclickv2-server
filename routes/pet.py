f282db604d4d289ebf5c82179593a12c2acca308
caceresrene <rene@outlook.my>
2022-09-05 23:32:37 -0300

    feat(pet): add read pets endpoint

--- routes/pet.py
+++ routes/pet.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, APIRouter
 from schemas.index import PetCreate, PetUpdate
 from sqlalchemy.orm import Session
-from controllers.index import create_pet, get_pets_by_owner, update_pet
+from controllers.index import create_pet, get_pets_by_owner, update_pet, get_pets
 from config.database import get_db
 
 router = APIRouter(prefix="/pet",  tags=["pet"])
@@ -13,8 +13,13 @@ def pet_create(pet: PetCreate, user_id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/")
+def read_pets(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return get_pets(db, skip=skip, limit=limit)
+
+
+@router.get("/{user_id}")
 def pets_by_owner(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return get_pets_by_owner(db, owner_id=user_id)
+    return get_pets_by_owner(db, owner_id=user_id, skip=skip, limit=limit)
 
 
 @router.put("/")
